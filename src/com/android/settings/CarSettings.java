82b0cda564a7c2cbd9659c27151bbb076a84a221
George <liushiwei.go@gmail.com>
2017-05-15 15:45:29 +0800

    update text

--- src/com/android/settings/CarSettings.java
+++ src/com/android/settings/CarSettings.java
@@ -10,7 +10,10 @@ public class CarSettings extends SettingsPreferenceFragment {
 	private static final String TAG = "CarSettings";
 
 	private static final String CAR_SETTINGS = "car_settings";
+	
+	private static final String REVERSE_SETTINGS = "reverse_settings";
 	private PreferenceScreen mCarSettings;
+	private PreferenceScreen mReverseSettings;
 
 	@Override
 	public void onCreate(Bundle icicle) {
@@ -19,6 +22,8 @@ public class CarSettings extends SettingsPreferenceFragment {
 		final PreferenceScreen screen = getPreferenceScreen();
 		mCarSettings = (PreferenceScreen) screen.findPreference(CAR_SETTINGS);
 		mCarSettings.setOnPreferenceClickListener(mClickListener);
+		mReverseSettings  = (PreferenceScreen) screen.findPreference(REVERSE_SETTINGS);
+		mReverseSettings.setOnPreferenceClickListener(mClickListener);
 	}
 
 	private OnPreferenceClickListener mClickListener = new OnPreferenceClickListener() {
@@ -29,6 +34,9 @@ public class CarSettings extends SettingsPreferenceFragment {
 			if (preference == mCarSettings) {
 //				getActivity().sendBroadcast(new Intent("com.george.settings.car_settings"));
 				getActivity().startActivity(new Intent("com.george.canbus.intent.action.OptionsActivity"));
+			}
+			if (preference == mReverseSettings) {
+				getActivity().sendBroadcast(new Intent("com.george.settings.reverse_settings"));
 			}
 			return false;
 		}
